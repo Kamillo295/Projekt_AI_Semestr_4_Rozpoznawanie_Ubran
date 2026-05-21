9bd27145799bff2d0f8b420cc28eebc9a2aa5e03
Kamillo295 <kammil2005@gmail.com>
2026-05-21 14:45:44 +0200

    123

--- Przepisane/Przepisane.py
+++ Przepisane/Przepisane.py
@@ -8,10 +8,10 @@ import numpy as np
 
 CONFIG = {
 
-    "batch_size": 128,
+    "batch_size": 64,
     "epochs": 30,
-    "learning_rate": 0.0005,
-    "random_seed": 42,    
+    "learning_rate": 0.0003,
+    "random_seed": 42,
 }
 
 def set_seed(seed):
@@ -19,10 +19,11 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
 
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
 
-class FunctionCNN(nn.Module):
+class FashionCNN(nn.Module):
 
     def __init__(self):
 
@@ -119,3 +120,104 @@ def get_data_loaders(batch_size):
 
     return train_loader, test_loader
 
+def evaluate_model(model, loader, device):
+    model.eval()
+
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        
+    accuracy = 100 * correct / total
+    return accuracy
+
+def main():
+    set_seed(CONFIG["random_seed"])
+
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+    print(f"\nUrzadzenie: {device}")
+    print(f"\nPobieranie danych...")
+
+    train_loader, test_loader = get_data_loaders(
+        CONFIG["batch_size"]
+        )
+
+    model = FashionCNN().to(device)
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = optim.Adam(
+        model.parameters(),
+        lr=CONFIG["learning_rate"]
+        )
+
+    print("\n Rozpoczynanie treningu\n")
+
+    best_accuracy = 0
+
+    for epoch in range(1, CONFIG["epochs"] + 1):
+        model.train()
+        running_loss = 0.0
+        correct_predictions = 0
+        total_samples = 0
+
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()     # liczy gradienty, czyli oblicza jak trzeba zmieniæ wagi modelu, ¿eby loss by³ mniejszy.
+            optimizer.step()    # aktualizacaja wag na podstawie obliczonego gradientu
+
+            running_loss += loss.item()
+            _, predicted = torch.max(outputs, 1)
+            total_samples += labels.size(0)
+            correct_predictions += (predicted == labels).sum().item()
+
+
+        epoch_loss = running_loss / len(train_loader)
+        epoch_accuracy = (
+            correct_predictions / total_samples
+        ) * 100
+
+        test_accuracy = evaluate_model( model, test_loader, device )
+
+        if test_accuracy > best_accuracy:
+            best_accuracy = test_accuracy
+            torch.save(
+                model.state_dict(),
+                "fashion_cnn_model.pth"
+                )
+
+        print(
+            f"Epoka {epoch:2d}/{CONFIG['epochs']} | "
+            f"Loss: {epoch_loss:.4f} | "
+            f"Train Accuracy: {epoch_accuracy:.2f}% | "
+            f"Test Accuracy: {test_accuracy:.2f}%"
+        )
+
+    print("\n====================================")
+    print("TRENING ZAKONCZONY")
+    print("====================================")
+
+    final_accuracy = evaluate_model(model, test_loader, device)
+
+    print(f"\nNajlepsza dokladnosc: {best_accuracy:.2f}%")
+    print("\nModel zapisany jako:")
+    print("fashion_cnn_model.pth")
+
+if __name__ == "__main__":
+    main()
